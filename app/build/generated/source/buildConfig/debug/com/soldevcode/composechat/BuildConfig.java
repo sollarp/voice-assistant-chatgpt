ab33683fe77fd83e6e262e30b101aabe848b7a70
sollarp <szpitor@gmail.com>
2023-08-26 13:00:21 +0100

    Updated git ignore

---
+++ app/build/generated/source/buildConfig/debug/com/soldevcode/composechat/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.soldevcode.composechat;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.soldevcode.composechat";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from build type: debug
+  public static final String GPT_KEY = "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";
+}
