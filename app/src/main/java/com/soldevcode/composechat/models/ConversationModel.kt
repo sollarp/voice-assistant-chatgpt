be025e602b9a4423fbebfe4550004b03a5969201
sollarp <szpitor@gmail.com>
2023-11-20 20:27:59 +0000

    Moved from liveData to stateFlow

--- app/src/main/java/com/soldevcode/composechat/models/ConversationModel.kt
+++ app/src/main/java/com/soldevcode/composechat/models/ConversationModel.kt
@@ -1,11 +1,16 @@
 package com.soldevcode.composechat.models
 
+import com.soldevcode.composechat.data.dto.gptRequest.ApiMessage
+import com.soldevcode.composechat.models.Message.Answer
+
 data class ConversationModel(
     var question: String = "",
     var answer: String ="",
     var chatId: String = "test",
     var chatOwner: String = "",
-)
+
+    )
+
 
 sealed interface Message {
     val text: String
@@ -13,12 +18,16 @@ sealed interface Message {
 
     data class Question(
         override val text: String,
-        override val timestamp: Long,
+        override val timestamp: Long = System.currentTimeMillis()
     ): Message
 
     data class Answer(
         override val text: String,
-        override val timestamp: Long,
+        override val timestamp: Long = System.currentTimeMillis(),
     ): Message
 }
 
+fun Message.toApiMessage(): ApiMessage = ApiMessage(
+    content = text,
+    role = if(this is Answer) "system" else "user",
+)
